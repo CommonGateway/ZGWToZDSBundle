7e8cbde3c22792341228b12d2d03989e298d4120
Robert Zondervan <robert@conduction.nl>
2023-05-22 16:50:33 +0200

    ZGW to ZDS identification action

--- src/Service/ZGWToZDSService.php
+++ src/Service/ZGWToZDSService.php
@@ -13,6 +13,7 @@ use CommonGateway\CoreBundle\Service\GatewayResourceService;
 use CommonGateway\CoreBundle\Service\MappingService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 
 class ZGWToZDSService
@@ -79,6 +80,7 @@ class ZGWToZDSService
         $this->data            = [];
         $this->mappingService  = $mappingService;
         $this->resourceService = $resourceService;
+        $this->callService     = $callService;
 
     }//end __construct()
 
@@ -103,19 +105,27 @@ class ZGWToZDSService
     }//end zgwToZdsHandler()
 
 
+    /**
+     * Creates a ZDS Di02 call to the ZDS source, and takes the identification in the respons as case identifier
+     *
+     * @param array $data          The data from the response.
+     * @param array $configuration The configuration for this action.
+     *
+     * @return array The resulting data array.
+     */
     public function zgwToZdsIdentificationHandler(array $data, array $configuration): array
     {
         $toMapping   = $this->resourceService->getMapping('https://zds.nl/mapping/zds.zgwZaakToDi02.mapping.json', 'common-gateway/zgw-to-zds-bundle');
         $fromMapping = $this->resourceService->getMapping('https://zds.nl/mapping/zds.Du02ToZgwZaak.mapping.json', 'common-gateway/zgw-to-zds-bundle');
         $source      = $this->resourceService->getSource('https://zds.nl/source/zds.source.json', 'common-gateway/zgw-to-zds-bundle');
 
-        $zaak = $this->entityManager->getRepository('App:ObjectEntity')->find($data['_id']);
+        $zaak = $this->entityManager->getRepository('App:ObjectEntity')->find(\Safe\json_decode($data['response']->getContent(), true)['_id']);
 
-        $zaakArray = $zaak->toArray();
+        $zaakArray = \Safe\json_decode($data['response']->getContent(), true);
 
-        $di02Message = $this->mappingService->mapping($zaakArray, $toMapping);
+        $di02Message = $this->mappingService->mapping($toMapping, $zaakArray);
 
-        $encoder = new XmlEncoder();
+        $encoder = new XmlEncoder(['xml_root_node_name' => 'SOAP-ENV:Envelope']);
         $message = $encoder->encode($di02Message, 'xml');
 
         $response = $this->callService->call($source, '/VrijeBerichten', 'POST', ['body' => $message]);
@@ -127,6 +137,8 @@ class ZGWToZDSService
 
         $this->entityManager->persist($zaak);
         $this->entityManager->flush();
+
+        $data['response'] = new Response(\Safe\json_encode($zaak->toArray()), 201, ['content-type' => 'application/json']); //TODO: This should become an SimXML response
 
         return $data;
 
