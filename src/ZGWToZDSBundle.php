fc861ec2d1664c27214c6118aa04dd0d45b76718
Sarai Misidjan <Sarai@conduction.nl>
2023-05-22 14:22:47 +0200

    fix namespaces

--- src/ZGWToZDSBundle.php
+++ src/ZGWToZDSBundle.php
@@ -6,7 +6,7 @@
  * @license EUPL-1.2 https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
  */
 
-namespace CommonGateway\PetStoreBundle;
+namespace CommonGateway\ZGWToZDSBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
